f262b0c2d418657af160470c12c3f5a573f792da
M A Hakim <hakimcseru@gmail.com>
2018-11-15 14:41:48 +0600

    remove version check

--- wp-scheduled-posts-pro.php
+++ wp-scheduled-posts-pro.php
@@ -11,46 +11,15 @@
  * Min WP Version: 2.5.0
  * Max WP Version: 4.8
  */
-
-    function getPluginVersion($allPlugins)
-    {
-        foreach($allPlugins as $plugins):
-            if($plugins['Name'] == "WP Scheduled Posts")
-                return $plugins['Version'];
-        endforeach;
-
-        return false;
-
-    }
     function admin_dependency_plugin()
     {
-    
-        $all_plugins = get_plugins();
-        if ( ! function_exists( 'get_plugins' ) ) {
-            require_once ABSPATH . 'wp-admin/includes/plugin.php';
-        }
-        $plugins=get_plugins();
-        $apl=get_option('active_plugins');
-        $activated_plugins=array();
-        foreach ($apl as $p){           
-            if(isset($plugins[$p])){
-                 array_push($activated_plugins, $plugins[$p]);
-            }           
-        }
-        $pluginVersion = getPluginVersion($activated_plugins);
-        
-        if(empty($pluginVersion))
+        if(!class_exists('wpsp_addon'))
         {
             die('"WP Scheduled Posts pro" requires "WP Scheduled Posts" Latest Plugin. Please install it.<a href="https://wordpress.org/plugins/wp-scheduled-posts/" target="_blank">WP Scheduled Posts</a>');
-        }elseif($pluginVersion < 2.0 && isset($pluginVersion))
-        {
-            die('Please Upgrade Your Version');
-
         }
         
+        
     }
-  
-
     register_activation_hook( __FILE__, 'admin_dependency_plugin' );
 
 
