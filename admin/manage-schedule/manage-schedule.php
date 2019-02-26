77211745b9d381ba81bf6d479e8df95bd807d13c
PriyoMukul <priyomukul@gmail.com>
2019-02-26 17:50:05 +0600

    Pro Check for change title

--- admin/manage-schedule/manage-schedule.php
+++ admin/manage-schedule/manage-schedule.php
@@ -1,8 +1,15 @@
 <?php
 
-
 add_action('admin_menu', function(){
-	add_submenu_page(pluginsFOLDER, __('Pro Setting'), __('Pro Setting'), "manage_options", 'wpsp-manage-schedule', 'wpsp_scheduled_options');
+	$active_plugins = get_option( 'active_plugins' );
+	$title = __( 'Pro Setting', 'wp-scheduled-posts' );
+	foreach( $active_plugins as $plugin ) {
+		if( $plugin === 'wp-scheduled-posts-pro/wp-scheduled-posts-pro.php' ) {
+			$title = __( 'Manage Schedule', 'wp-scheduled-posts' );
+		}
+	}
+
+	add_submenu_page(pluginsFOLDER, $title, $title, "manage_options", 'wpsp-manage-schedule', 'wpsp_scheduled_options');
 });
 
 
