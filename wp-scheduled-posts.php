b37078f0a4bb45ee1d844c654f8459364ca53202
Tushar Imran <tusharimran9909@gmail.com>
2020-12-06 11:54:59 +0600

    update plugin name

--- wp-scheduled-posts.php
+++ wp-scheduled-posts.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Plugin Name: WP Scheduled Posts
+ * Plugin Name: SchedulePress
  * Description: Automate your content workflow with SchedulePress. Take a quick glance at your content planning with Schedule Calendar, Dashboard widget & Sitewide admin bar. Instantly share your posts on social media platforms such as Facebook, Twitter & many more.
  * Version: 3.5
  * Author: WPDeveloper
