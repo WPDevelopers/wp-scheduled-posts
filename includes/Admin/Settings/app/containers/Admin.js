50e7540d7a47bd4a89babc76b5fdddc25c2bcd5e
Tushar Imran <tusharimran9909@gmail.com>
2020-12-13 11:07:58 +0600

    wp 5.6 notice style broken issue fixing

--- includes/Admin/Settings/app/containers/Admin.js
+++ includes/Admin/Settings/app/containers/Admin.js
@@ -3,14 +3,14 @@ import Setting from './Settings'
 import Topbar from '../components/Topbar'
 const Admin = ({ wpspObject }) => {
     return (
-        <div className='wrap'>
+        <React.Fragment>
             <Topbar
                 pluginRootUri={wpspObject.plugin_root_uri}
                 freeVersion={wpspObject.free_version}
                 proVersion={wpspObject.pro_version}
             />
             <Setting wpspObject={wpspObject} />
-        </div>
+        </React.Fragment>
     )
 }
 export default Admin
