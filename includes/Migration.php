447d6e3a06663e41d18351b4fa50d9ee46a764f5
Tushar Imran <tusharimran9909@gmail.com>
2021-01-11 15:02:31 +0600

    bug fix - migration social template twitter, linkedin and pinterest

--- includes/Migration.php
+++ includes/Migration.php
@@ -145,54 +145,54 @@ class Migration {
                 // social template - twitter
                 $wpscp_twitter_template_structure = get_option('wpscp_twitter_template_structure');
                 if (!empty($wpscp_twitter_template_structure)) {
-                    $settings['social_templates']->twitter[0]['template_structure'] = $wpscp_twitter_template_structure;
+                    $settings['social_templates']['twitter'][0]['template_structure'] = $wpscp_twitter_template_structure;
                 }
                 $wpscp_twitter_template_category_tags_support = get_option('wpscp_twitter_template_category_tags_support');
                 if (!empty($wpscp_twitter_template_category_tags_support)) {
-                    $settings['social_templates']->twitter[1]['is_category_as_tags'] = $wpscp_twitter_template_category_tags_support;
+                    $settings['social_templates']['twitter'][1]['is_category_as_tags'] = $wpscp_twitter_template_category_tags_support;
                 }
                 $wpscp_twitter_template_thumbnail = get_option('wpscp_twitter_template_thumbnail');
                 if (!empty($wpscp_twitter_template_thumbnail)) {
-                    $settings['social_templates']->twitter[2]['is_show_post_thumbnail'] = $wpscp_twitter_template_thumbnail;
+                    $settings['social_templates']['twitter'][2]['is_show_post_thumbnail'] = $wpscp_twitter_template_thumbnail;
                 }
                 $wpscp_twitter_content_source = get_option('wpscp_twitter_content_source');
                 if (!empty($wpscp_twitter_content_source)) {
-                    $settings['social_templates']->twitter[3]['content_source'] = $wpscp_twitter_content_source;
+                    $settings['social_templates']['twitter'][3]['content_source'] = $wpscp_twitter_content_source;
                 }
                 $wpscp_twitter_tweet_limit = get_option('wpscp_twitter_tweet_limit');
                 if (!empty($wpscp_twitter_tweet_limit)) {
-                    $settings['social_templates']->twitter[4]['tweet_limit'] = $wpscp_twitter_tweet_limit;
+                    $settings['social_templates']['twitter'][4]['tweet_limit'] = $wpscp_twitter_tweet_limit;
                 }
 
                 // social template - linkedin
                 $wpscp_pro_linkedin_content_type = get_option('wpscp_pro_linkedin_content_type');
                 if (!empty($wpscp_pro_linkedin_content_type)) {
-                    $settings['social_templates']->linkedin[0]['content_type'] = $wpscp_pro_linkedin_content_type;
+                    $settings['social_templates']['linkedin'][0]['content_type'] = $wpscp_pro_linkedin_content_type;
                 }
                 $wpscp_pro_liinkedin_template_category_tags_support = get_option('wpscp_pro_liinkedin_template_category_tags_support');
                 if (!empty($wpscp_pro_liinkedin_template_category_tags_support)) {
-                    $settings['social_templates']->linkedin[1]['is_category_as_tags'] = $wpscp_pro_liinkedin_template_category_tags_support;
+                    $settings['social_templates']['linkedin'][1]['is_category_as_tags'] = $wpscp_pro_liinkedin_template_category_tags_support;
                 }
                 $wpscp_pro_linkedin_content_source = get_option('wpscp_pro_linkedin_content_source');
                 if (!empty($wpscp_pro_linkedin_content_source)) {
-                    $settings['social_templates']->linkedin[2]['content_source'] = $wpscp_pro_linkedin_content_source;
+                    $settings['social_templates']['linkedin'][2]['content_source'] = $wpscp_pro_linkedin_content_source;
                 }
                 $wpscp_pro_linkedin_template_structure = get_option('wpscp_pro_linkedin_template_structure');
                 if (!empty($wpscp_pro_linkedin_template_structure)) {
-                    $settings['social_templates']->linkedin[3]['template_structure'] = $wpscp_pro_linkedin_template_structure;
+                    $settings['social_templates']['linkedin'][3]['template_structure'] = $wpscp_pro_linkedin_template_structure;
                 }
                 $wpscp_pro_linkedin_status_limit = get_option('wpscp_pro_linkedin_status_limit');
                 if (!empty($wpscp_pro_linkedin_status_limit)) {
-                    $settings['social_templates']->linkedin[4]['status_limit'] = $wpscp_pro_linkedin_status_limit;
+                    $settings['social_templates']['linkedin'][4]['status_limit'] = $wpscp_pro_linkedin_status_limit;
                 }
                 // social template - pinterest
                 $pinterest = get_option('wpscp_pro_pinterest_template_settings');
                 if ($pinterest !== false && is_array($pinterest)) {
-                    $settings['social_templates']->pinterest[0]['is_set_image_link'] = $pinterest['add_image_link'];
-                    $settings['social_templates']->pinterest[1]['is_category_as_tags'] = $pinterest['template_category_tags_support'];
-                    $settings['social_templates']->pinterest[2]['content_source'] = $pinterest['content_source'];
-                    $settings['social_templates']->pinterest[3]['template_structure'] = $pinterest['template_structure'];
-                    $settings['social_templates']->pinterest[4]['note_limit'] = $pinterest['pin_note_limit'];
+                    $settings['social_templates']['pinterest'][0]['is_set_image_link'] = $pinterest['add_image_link'];
+                    $settings['social_templates']['pinterest'][1]['is_category_as_tags'] = $pinterest['template_category_tags_support'];
+                    $settings['social_templates']['pinterest'][2]['content_source'] = $pinterest['content_source'];
+                    $settings['social_templates']['pinterest'][3]['template_structure'] = $pinterest['template_structure'];
+                    $settings['social_templates']['pinterest'][4]['note_limit'] = $pinterest['pin_note_limit'];
                 }
                 if (!empty($settings)) {
                     update_option(WPSP_SETTINGS_NAME, json_encode($settings));
