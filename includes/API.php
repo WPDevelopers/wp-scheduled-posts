e3a313ed73c7d1e3169220fd8df2300f0e0b1ee8
Tushar Imran <tusharimran9909@gmail.com>
2021-01-31 11:06:48 +0600

    bug fix - jwt compatible issue

--- includes/API.php
+++ includes/API.php
@@ -9,9 +9,15 @@ class API
     public function __construct()
     {
         $this->load_settings_API();
+        add_filter('jwt_auth_whitelist', array($this, 'whitelist_API'));
     }
     public function load_settings_API()
     {
         API\Settings::get_instance();
+    }
+    public function whitelist_API($endpoints)
+    {
+        $endpoints[] = '/wp-json/wp-scheduled-posts/v1/*';
+        return $endpoints;
     }
 }
