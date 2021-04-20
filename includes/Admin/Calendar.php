6ad60465c57810a5ee7890093613fe9617b2d511
Tushar Imran <tusharimran9909@gmail.com>
2021-04-20 11:20:39 +0600

    bug fix - calendar time issue fixing

--- includes/Admin/Calendar.php
+++ includes/Admin/Calendar.php
@@ -110,11 +110,11 @@ class Calendar
         $postTitle = (isset($_POST['postTitle']) ? $_POST['postTitle'] : '');
         $postContent = (isset($_POST['postContent']) ? $_POST['postContent'] : '');
 
-        if ($dateStr != "Invalid Date" && empty($postid)) {
+        if ($dateStr != "Invalid Date" && !empty($postid)) {
             $postdate = new \DateTime(substr($dateStr, 0, 25));
             $postdateformat = $postdate->format('Y-m-d H:i:s');
             $postdate_gmt = ($postdateformat != "" ? gmdate('Y-m-d H:i:s', strtotime($postdateformat)) : '');
-        } else if ($dateStr != "Invalid Date" && !empty($postid)) {
+        } else if ($dateStr != "Invalid Date" && empty($postid)) {
             $default_schedule_time = '12:00 am';
             if (!empty($calendar_schedule_time)) {
                 $default_schedule_time = $calendar_schedule_time;
