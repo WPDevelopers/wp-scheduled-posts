0e90c2479056cbc46c8d9b82e44cee8b5ab5fd2d
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2022-08-07 17:41:00 +0600

    Publish future post

--- assets/gutenberg/index.js
+++ assets/gutenberg/index.js
@@ -20,18 +20,19 @@ import PublishFutureButton from "./publish-future-button";
 class AdminPublishButton extends Component {
   constructor(props) {
     super(props);
-		this.handleChange = this.handleChange.bind(this);
-		this.state = {
-			showHelp : false,
-			publishImmediately : false,
-		}
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      showHelp: false,
+      publishImmediately: false,
+    };
   }
 
-	handleChange( checked ) {
-
-	}
+  handleChange(checked) {}
   render() {
-    if (publish_button_off == "" || !(this.props.isScheduled && ! this.props.isPublished)) {
+    if (
+      publish_button_off == "" ||
+      !(this.props.isScheduled && !this.props.isPublished)
+    ) {
       return "";
     }
 
@@ -42,17 +43,28 @@ class AdminPublishButton extends Component {
           {/*  style={{display: 'flex', alignItems: 'center', gap: 5}} */}
           <div>
             <CheckboxControl
-                label="Publish future post immediately"
-                checked={ this.state.publishImmediately }
-                onChange={ (checked) => this.setState({publishImmediately : checked}) }
+              label="Publish future post immediately"
+              checked={this.state.publishImmediately}
+              onChange={(checked) => {
+                this.setState({ publishImmediately: checked });
+                if (checked == false) {
+                  this.setState({ showHelp: false });
+                }
+              }}
             />
-            <a id="wpscp-future-post-help-handler" href="javascript:void();" title="Show/Hide Help" onClick={() => {
-              this.setState({showHelp: !this.state.showHelp})
-            }}>(?)</a>
+            <a
+              id="wpscp-future-post-help-handler"
+              href="javascript:void();"
+              title="Show/Hide Help"
+              onClick={() => {
+                this.setState({ showHelp: !this.state.showHelp });
+              }}
+            >
+              (?)
+            </a>
           </div>
-          {
-            this.state.publishImmediately &&
-            <div style={{display: 'flex', flexWrap: 'wrap', alignItems: 'center', gap: 10}}>
+          {this.state.publishImmediately && (
+            <div className="sc-publish-future-buttons">
               <PublishButton
                 {...this.props}
                 currentTime={currentTime}
@@ -64,14 +76,16 @@ class AdminPublishButton extends Component {
                 publish={publishFutureDate}
               />
             </div>
-          }
-          {
-            this.state.showHelp &&
-            <div>If you schedule this post and check this option then your post will be published immediately but post date-time will not set current date. Post date-time will be your scheduled future date-time.</div>
-          }
+          )}
+          {this.state.showHelp && (
+            <div style={{ marginTop: 5 }}>
+              If you schedule this post and check this option then your post
+              will be published immediately but post date-time will not set
+              current date. Post date-time will be your scheduled future
+              date-time.
+            </div>
+          )}
         </div>
-
-
       </PluginPostStatusInfo>
     );
   }
@@ -100,15 +114,19 @@ export default compose([
     return {
       editPost(newMeta) {
         var new_date = JSON.parse(newMeta);
-		if( typeof new_date === 'string' ) {
+        if (typeof new_date === "string") {
           new_date = JSON.parse(new_date);
         }
-		editPost( { date: new_date.date, date_gmt: new_date.date_gmt, status: new_date.status } );
+        editPost({
+          date: new_date.date,
+          date_gmt: new_date.date_gmt,
+          status: new_date.status,
+        });
       },
       setMetaValue: function (metaValue) {
         editPost({ meta: metaValue });
       },
-	  savePost,
+      savePost,
     };
   }),
   ifCondition(({ postType }) => {
