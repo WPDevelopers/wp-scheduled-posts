fbe825aab445302a69f0ea417a15dc0aef0bbc0c
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2022-09-19 17:41:57 +0600

    updated composer

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -17,7 +17,7 @@ return array(
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
     'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
     'Facebook\\' => array($vendorDir . '/facebook/graph-sdk/src/Facebook'),
-    'DirkGroenen\\Pinterest\\' => array($vendorDir . '/dirkgroenen/pinterest-api-php/src/Pinterest'),
+    'DirkGroenen\\Pinterest\\' => array($vendorDir . '/wpdevelopers/pinterest-api-php/src/Pinterest'),
     'Composer\\CaBundle\\' => array($vendorDir . '/composer/ca-bundle/src'),
     'Abraham\\TwitterOAuth\\' => array($vendorDir . '/abraham/twitteroauth/src'),
 );
--- vendor/wpdevelopers/pinterest-api-php/src/Pinterest/Pinterest.php
+++ vendor/wpdevelopers/pinterest-api-php/src/Pinterest/Pinterest.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Copyright 2015 Dirk Groenen
  *
@@ -23,8 +22,7 @@ use DirkGroenen\Pinterest\Exceptions\InvalidEndpointException;
  * @property \DirkGroenen\Pinterest\Endpoints\Users users
  * @property \DirkGroenen\Pinterest\Endpoints\Sections sections
  */
-class Pinterest
-{
+class Pinterest {
 
     /**
      * Reference to authentication class instance
