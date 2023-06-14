303bd42eff8461d4e87b7b78f3d7250f2fee200d
shuvo247 <78678198+shuvo247@users.noreply.github.com>
2023-06-14 17:24:29 +0600

    sidebar and features section

---
+++ includes/Admin/Settings/app/Settings/fields/Features.tsx
@@ -0,0 +1,28 @@
+import classNames from 'classnames';
+import React from 'react'
+
+const Features = (props) => {
+
+    const { heading, button_text, button_link, options } = props?.content;
+    return (
+        <div className={classNames('wprf-control', 'wprf-features', `wprf-${props.name}-features`, props?.classes)}>
+            <div className='header'>
+                <h3>{heading}</h3>
+                <a href={button_link}>{button_text}</a>
+            </div>
+            <div className='content'>
+                {options?.map((item) => (
+                    <a href={item?.link} target="_blank" key={Math.random()}>
+                        <div className='single-content'>
+                            <i className="wpsp wpsp-something"></i>
+                            <h5>{item?.title}</h5>
+                        </div>
+                    </a>
+                ))}
+
+            </div>
+        </div>
+    )
+}
+
+export default Features;
--- includes/Admin/Settings/assets/js/admin.asset.php
+++ includes/Admin/Settings/assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-i18n', 'wp-media-utils'), 'version' => '4cd8eb31eef653dee8e1');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-i18n', 'wp-media-utils'), 'version' => 'c74d76cc2630b612c730');
