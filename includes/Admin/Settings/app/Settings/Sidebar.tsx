b7b57216e2f618cdcab40bfe93d35cf539fc09c3
Kazi Sourav <khmsourav@gmail.com>
2023-06-15 19:15:40 +0600

    Social Template related scss

--- includes/Admin/Settings/app/Settings/Sidebar.tsx
+++ includes/Admin/Settings/app/Settings/Sidebar.tsx
@@ -8,6 +8,7 @@ const Sidebar = ({ props }) => {
         <div className="wpsp-admin-sidebar">
             <div className='upgrade-pro card'>
                 {/* <img className='icon-wrapper' src={upgradePro} alt='icon-1' /> */}
+                {/* @ts-ignore */}
                 <img src={`${wpspSettingsGlobal?.image_path}upgrade-pro.png`} alt="upgrade-pro-img" />
                 <h3>Get Unlimited Features</h3>
                 <p>Supercharge your content schedule and have a peace in mind</p>
@@ -15,6 +16,7 @@ const Sidebar = ({ props }) => {
             </div>
             <div className='card'>
                 {/* <img className='icon-wrapper' src={docIcon} alt='icon-1' /> */}
+                {/* @ts-ignore */}
                 <img src={`${wpspSettingsGlobal?.image_path}doc.png`} alt="doc-icon" />
                 <h3>Documentation</h3>
                 <p>Get started spending some time with the documentation to get familiar with SchedulePress.</p>
@@ -22,6 +24,7 @@ const Sidebar = ({ props }) => {
             </div>
             <div className='card'>
                 {/* <img className='icon-wrapper' src={docIcon} alt='icon-1' /> */}
+                {/* @ts-ignore */}
                 <img src={`${wpspSettingsGlobal?.image_path}contribute-icon.png`} alt="contribute-icon" />
                 <h3>Contribute to SchedulePress</h3>
                 <p>You can contribute to making SchedulePress better by reporting bugs</p>
@@ -29,6 +32,7 @@ const Sidebar = ({ props }) => {
             </div>
             <div className='card'>
                 {/* <img className='icon-wrapper' src={docIcon} alt='icon-1' /> */}
+                {/* @ts-ignore */}
                 <img src={`${wpspSettingsGlobal?.image_path}help.png`} alt="help-icon" />
                 <h3>Need Help?</h3>
                 <p>Stuck with something? Get help from the community WPDeveloper Forum or Facebook Community.</p>
@@ -36,6 +40,7 @@ const Sidebar = ({ props }) => {
             </div>
             <div className='card'>
                 {/* <img className='icon-wrapper' src={docIcon} alt='icon-1' /> */}
+                {/* @ts-ignore */}
                 <img src={`${wpspSettingsGlobal?.image_path}love-icon.png`} alt="love-icon" />
                 <h3>Show your Love</h3>
                 <p>We love to have you in the SchedulePress family. We are making it more awesome every day.</p>
--- includes/Admin/Settings/assets/js/admin.asset.php
+++ includes/Admin/Settings/assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-i18n', 'wp-media-utils'), 'version' => '7da5d347ac970d00fdb3');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-i18n', 'wp-media-utils'), 'version' => '7c252a6f08e36370cbe6');
