3bf09b17b1fa06bf99aaa3b2196b97191e60334f
Kazi Sourav <khmsourav@gmail.com>
2023-06-21 11:23:46 +0600

    Sidebar & Pro Freatures Related style updated

--- includes/Admin/Settings/assets/js/admin.asset.php
+++ includes/Admin/Settings/assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-i18n', 'wp-media-utils'), 'version' => 'f07742ac79edda2d1917');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-i18n', 'wp-media-utils'), 'version' => '392a182cb68599a4511d');
