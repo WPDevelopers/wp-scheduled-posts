37377647632558932d7fade1a0499e708b2dc26f
Kazi Sourav <khmsourav@gmail.com>
2023-07-06 16:22:30 +0600

    some icon added

--- includes/Admin/Settings/assets/js/admin.asset.php
+++ includes/Admin/Settings/assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-i18n', 'wp-media-utils'), 'version' => 'bb438ad86e92e89b678c');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-i18n', 'wp-media-utils'), 'version' => 'bcbd485463cbe8044393');
