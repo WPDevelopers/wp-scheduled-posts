0a2fe77f86c4565dd2824403263d98d7269d82da
Kazi Sourav <khmsourav@gmail.com>
2023-07-13 15:13:34 +0600

    Calendar sidebar icon style update

--- includes/Admin/Settings/assets/js/admin.asset.php
+++ includes/Admin/Settings/assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => '2a11d8314a6e2099ef13');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => 'b5e62ba734fb8914184c');
