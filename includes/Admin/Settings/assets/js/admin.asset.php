fa5fe74926b6c731f31a2668cec05bb71271792d
Kazi Sourav <khmsourav@gmail.com>
2023-07-16 02:27:47 +0600

    modal styling

--- includes/Admin/Settings/assets/js/admin.asset.php
+++ includes/Admin/Settings/assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => '70a2441c835ac0a23aa6');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => 'a48bef5aa40e7e714e10');
