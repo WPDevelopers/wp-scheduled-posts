3eec0f3be119e037b9a99c4d61e711607499f39e
shuvo247 <78678198+shuvo247@users.noreply.github.com>
2023-07-23 11:36:43 +0600

    Sidebar license management content & condition added & Info text added

--- includes/Admin/Settings/app/Settings/Sidebar.tsx
+++ includes/Admin/Settings/app/Settings/Sidebar.tsx
@@ -10,40 +10,59 @@ const Sidebar = ({ props }) => {
     if(props.id !== 'tab-sidebar-layout' || builderContext.config.active === 'layout_calendar' ) {
         return;
     }
+    {/* @ts-ignore */}
+    const is_pro = wpspSettingsGlobal.pro_version;
 
     return (
         <div className="wpsp-admin-sidebar">
-            <div className='upgrade-pro card'>
-                {/* <img className='icon-wrapper' src={upgradePro} alt='icon-1' /> */}
-                {/* @ts-ignore */}
-                <img src={`${wpspSettingsGlobal?.image_path}upgrade-pro.png`} alt={__('upgrade-pro-img','wp-scheduled-posts')} />
-                <h3>Get Unlimited Features</h3>
-                <p>Supercharge your content schedule and have a peace in mind</p>
-                <button><span>Upgrade To Pro</span></button>
-            </div>
+            {
+                is_pro && (
+                <div className='manage-license card'>
+                    {/* @ts-ignore */}
+                    <img src={`${wpspSettingsGlobal?.image_path}upgrade-pro-new.png`} alt={__('upgrade-pro-img','wp-scheduled-posts')} />
+                    <h3>{__('Manage License','wp-scheduled-posts')}</h3>
+                    <p>{ __('Supercharge your content schedule and ave a peace in mind','wp-scheduled-posts') }</p>
+                    <a href='#'><span>{__('Manage License','wp-scheduled-posts')}</span></a>
+                </div>
+                )
+            }
+            {
+                !is_pro && (
+                <div className='upgrade-pro card'>
+                    {/* <img className='icon-wrapper' src={upgradePro} alt='icon-1' /> */}
+                    {/* @ts-ignore */}
+                    <img src={`${wpspSettingsGlobal?.image_path}upgrade-pro-new.png`} alt={__('upgrade-pro-img','wp-scheduled-posts')} />
+                    <h3>{__('Get Unlimited Features','wp-scheduled-posts')}</h3>
+                    <p>{ __('Supercharge your content schedule and have a peace in mind','wp-scheduled-posts') }</p>
+                    <a href='#'><span>{__('Upgrade To Pro','wp-scheduled-posts')}</span></a>
+                </div>
+                )
+            }
+            
+            
             <div className='card'>
                 <i className="wpsp-icon wpsp-file"></i>
-                <h3>Documentation</h3>
-                <p>Get started spending some time with the documentation to get familiar with SchedulePress.</p>
-                <button>Documentation</button>
+                <h3>{__('Documentation','wp-scheduled-posts')}</h3>
+                <p>{__('Get started spending some time with the documentation to get familiar with SchedulePress.','wp-scheduled-posts')}</p>
+                <a href='#'>{__('Documentation','wp-scheduled-posts')}</a>
             </div>
             <div className='card'>
                 <i className="wpsp-icon wpsp-puzzle"></i>
-                <h3>Contribute to SchedulePress</h3>
-                <p>You can contribute to making SchedulePress better by reporting bugs</p>
-                <button>Report A Bug</button>
+                <h3>{__('Contribute to SchedulePress','wp-scheduled-posts')}</h3>
+                <p>{__('You can contribute to making SchedulePress better by reporting bugs','wp-scheduled-posts')}</p>
+                <a href='#'>{__('Report A Bug','wp-scheduled-posts')}</a>
             </div>
             <div className='card'>
                 <i className="wpsp-icon wpsp-comment"></i>
-                <h3>Need Help?</h3>
-                <p>Stuck with something? Get help from the community WPDeveloper Forum or Facebook Community.</p>
-                <button>Get Support</button>
+                <h3>{__('Need Help?','wp-scheduled-posts')}</h3>
+                <p>{__('Stuck with something? Get help from the community WPDeveloper Forum or Facebook Community.','wp-scheduled-posts')}</p>
+                <a href='#'>{__('Get Support','wp-scheduled-posts')}</a>
             </div>
             <div className='card'>
                 <i className="wpsp-icon wpsp-chat-2"></i>
-                <h3>Show your Love</h3>
-                <p>We love to have you in the SchedulePress family. We are making it more awesome every day.</p>
-                <button>Show your Love</button>
+                <h3>{__('Show your Love','wp-scheduled-posts')}</h3>
+                <p>{__('We love to have you in the SchedulePress family. We are making it more awesome every day.','wp-scheduled-posts')}</p>
+                <a href='#'>{__('Show your Love','wp-scheduled-posts')}</a>
             </div>
         </div>
     );
