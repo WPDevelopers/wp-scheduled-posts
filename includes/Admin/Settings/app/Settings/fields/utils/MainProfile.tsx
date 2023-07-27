f64b87616f213ffebd70da41278c297b37ee76b8
Mozammel Ali <sapanmozammel@gmail.com>
2023-07-27 11:59:49 +0600

    select disable for pro feature related style done.

--- includes/Admin/Settings/app/Settings/fields/utils/MainProfile.tsx
+++ includes/Admin/Settings/app/Settings/fields/utils/MainProfile.tsx
@@ -1,97 +1,132 @@
 import { __ } from '@wordpress/i18n';
 import React, { useState } from 'react';
-import Select from "react-select";
+import Select, { components } from 'react-select';
 import { selectStyles } from '../../helper/styles';
 
-export default function MainProfile( { props, handleProfileStatusChange, profileStatus, openApiCredentialsModal } ) {    
-    let options = [];
-    // @ts-ignore
-    let pageDisabled = wpspSettingsGlobal?.pro_version ? false : true;
-    let currentActiveAccountType = localStorage.getItem('account_type');
+// Prepare options with checkbox
+const Option = (props) => {
+  return (
+    <components.Option {...props}>
+      <span className="wprf-select-pro-option">{props.label}</span>
+    </components.Option>
+  );
+};
 
-    if( props?.type == 'facebook' ) {
-        options = [
-            { value: 'page',    label: __('Page','wp-scheduled-posts'), selected : currentActiveAccountType == 'page' ? true : false },
-            { value: 'group',   label: __('Group','wp-scheduled-posts'), selected : currentActiveAccountType == 'group' ? true : false }
-        ]
-    }else{
-        options = [
-            { value: 'profile',    label: __('Profile','wp-scheduled-posts'), selected : currentActiveAccountType == 'profile' ? true : false },
-            { value: 'page',   label: __('Page','wp-scheduled-posts'), isDisabled: pageDisabled, selected : currentActiveAccountType == 'page' ? true : false }
-        ]
-    }
+export default function MainProfile({
+  props,
+  handleProfileStatusChange,
+  profileStatus,
+  openApiCredentialsModal,
+}) {
+  let options = [];
+  // @ts-ignore
+  let pageDisabled = wpspSettingsGlobal?.pro_version ? false : true;
+  let currentActiveAccountType = localStorage.getItem('account_type');
 
-    const [accountType,setAccountType] = useState(options[0].value);
-    const handleAccountType = (selectedOption) => {
-        setAccountType(selectedOption.value)
-    };
+  if (props?.type == 'facebook') {
+    options = [
+      {
+        value: 'page',
+        label: __('Page', 'wp-scheduled-posts'),
+        selected: currentActiveAccountType == 'page' ? true : false,
+      },
+      {
+        value: 'group',
+        label: __('Group', 'wp-scheduled-posts'),
+        selected: currentActiveAccountType == 'group' ? true : false,
+      },
+    ];
+  } else {
+    options = [
+      {
+        value: 'profile',
+        label: __('Profile', 'wp-scheduled-posts'),
+        selected: currentActiveAccountType == 'profile' ? true : false,
+      },
+      {
+        value: 'page',
+        label: __('Page', 'wp-scheduled-posts'),
+        isDisabled: pageDisabled,
+        selected: currentActiveAccountType == 'page' ? true : false,
+      },
+    ];
+  }
 
-    const mainSelectStyles = { ...selectStyles, control: (base, state) => ({
-        ...base,
-        boxShadow: "none", 
-        borderColor: "#D7DBDF",
-        backgroundColor: "#F9FAFC",
-        color: "#6E6E8D",
-        "&:hover": {
-            borderColor: "#cccccc"
-        }
-      }), }
-    
-    return (
-        <>
-            <div className="card-header">
-                <div className="heading">
-                    <img width={'30px'} src={`${props?.logo}`} alt={`${props?.label}`} />
-                    <h5>{props?.label}</h5>
-                </div>
-                <div className="status">
-                    <div className="switcher">
-                        <input
-                            id={props?.id}
-                            type='checkbox'
-                            checked={profileStatus}
-                            className="wprf-switcher-checkbox"
-                            onChange={(event) =>
-                                handleProfileStatusChange(event)
-                            }
-                        />
-                        <label
-                            className="wprf-switcher-label"
-                            htmlFor={props?.id}
-                            style={{ background: profileStatus && '#02AC6E' }}
-                        >
-                            <span className={`wprf-switcher-button`} />
-                        </label>
-                    </div>
-                </div>
-            </div>
-            <div className="card-content">
-                <p dangerouslySetInnerHTML={{ __html: props?.desc }} />
-            </div>
-            <div className="card-footer">
-                { ['facebook', 'linkedin'].includes(props?.type) && (
-                    <Select
-                        id={props?.id}
-                        onChange={(event) => {
-                            handleAccountType(event);
-                        }}
-                        options={options}
-                        defaultValue={options[0]}
-                        className='main-select'
-                        styles={mainSelectStyles}
-                        classNamePrefix='social-media-type-select'
-                    />
-                ) }
-                <button
-                    type="button"
-                    className={
-                    "wpscp-social-tab__btn--addnew-profile"
-                    }
-                    onClick={() => openApiCredentialsModal(accountType)}
-                    >
-                    { __("Add New", "wp-scheduled-posts")}
-                </button>
-            </div>
-        </>
-    )
+  const [accountType, setAccountType] = useState(options[0].value);
+  const handleAccountType = (selectedOption) => {
+    setAccountType(selectedOption.value);
+  };
+
+  const mainSelectStyles = {
+    ...selectStyles,
+    control: (base, state) => ({
+      ...base,
+      boxShadow: 'none',
+      borderColor: '#D7DBDF',
+      backgroundColor: '#F9FAFC',
+      color: '#6E6E8D',
+      '&:hover': {
+        borderColor: '#cccccc',
+      },
+    }),
+  };
+
+  return (
+    <>
+      <div className="card-header">
+        <div className="heading">
+          <img
+            width={'30px'}
+            src={`${props?.logo}`}
+            alt={`${props?.label}`}
+          />
+          <h5>{props?.label}</h5>
+        </div>
+        <div className="status">
+          <div className="switcher">
+            <input
+              id={props?.id}
+              type="checkbox"
+              checked={profileStatus}
+              className="wprf-switcher-checkbox"
+              onChange={(event) => handleProfileStatusChange(event)}
+            />
+            <label
+              className="wprf-switcher-label"
+              htmlFor={props?.id}
+              style={{ background: profileStatus && '#02AC6E' }}>
+              <span className={`wprf-switcher-button`} />
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="card-content">
+        <p dangerouslySetInnerHTML={{ __html: props?.desc }} />
+      </div>
+      <div className="card-footer">
+        {['facebook', 'linkedin'].includes(props?.type) && (
+          <Select
+            id={props?.id}
+            onChange={(event) => {
+              handleAccountType(event);
+            }}
+            components={{
+              Option,
+            }}
+            options={options}
+            defaultValue={options[0]}
+            className="main-select"
+            styles={mainSelectStyles}
+            classNamePrefix="social-media-type-select"
+          />
+        )}
+        <button
+          type="button"
+          className={'wpscp-social-tab__btn--addnew-profile'}
+          onClick={() => openApiCredentialsModal(accountType)}>
+          {__('Add New', 'wp-scheduled-posts')}
+        </button>
+      </div>
+    </>
+  );
 }
