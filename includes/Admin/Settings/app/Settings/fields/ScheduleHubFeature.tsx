38914ba119b05ef803a19862ce413641701463a3
shuvo247 <78678198+shuvo247@users.noreply.github.com>
2023-07-27 12:57:51 +0600

    remove pro_feature class if pro is active & scheduling hub features desing issue fixed

--- includes/Admin/Settings/app/Settings/fields/ScheduleHubFeature.tsx
+++ includes/Admin/Settings/app/Settings/fields/ScheduleHubFeature.tsx
@@ -6,69 +6,36 @@ const ScheduleHubFeature = (props) => {
 // @ts-ignore 
 let is_pro = wpspSettingsGlobal?.pro_version ? true : false;
   return (
-<<<<<<< Updated upstream
-    <div
-      className={classNames(
-        'wprf-control',
-        'wprf-schedule-hub-features',
-        `wprf-${props.name}-schedule-hub-features`,
-        props?.classes
-      )}>
+    <>
     { !is_pro && (
-        <>
-            <div className="wprf-heading">
-                <h2>Upgrade to use these Pro Features</h2>
-                <a href="#">Upgrade To Pro</a>
-            </div>
-            <div className="wprf-card-wrapper">
-                <div className="single-card">
-                    <i className="wpsp-icon wpsp-advance-pro"></i>
-                    <h4>Manage Schedule</h4>
-                </div>
-                <div className="single-card">
-                    <i className="wpsp-icon wpsp-manage-pro"></i>
-                    <h4>Advanced Schedule</h4>
-                </div>
-                <div className="single-card">
-                    <i className="wpsp-icon wpsp-missed-pro"></i>
-                    <h4>Missed Schedule</h4>
-                </div>
-            </div>
-        </>
-    ) }
-    </div>
-=======
-      <>
-        { !is_pro && (
-            <div
-            className={classNames(
-              'wprf-control',
-              'wprf-scheudle-hub-features',
-              `wprf-${props.name}-schedule-hub-features`,
-              props?.classes
-            )}>
-              <div className="wprf-heading">
-                  <h2>Upgrade to use these Pro Features</h2>
-                  <a href="#">Upgrade To Pro</a>
+        <div
+        className={classNames(
+          'wprf-control',
+          'wprf-schedule-hub-features',
+          `wprf-${props.name}-schedule-hub-features`,
+          props?.classes
+        )}>
+          <div className="wprf-heading">
+              <h2>Upgrade to use these Pro Features</h2>
+              <a href="#">Upgrade To Pro</a>
+          </div>
+          <div className="wprf-card-wrapper">
+              <div className="single-card">
+                  <i className="wpsp-icon wpsp-advance-pro"></i>
+                  <h4>Manage Schedule</h4>
               </div>
-              <div className="wprf-card-wrapper">
-                  <div className="single-card">
-                      <i>Icon</i>
-                      <h4>Manage Schedule</h4>
-                  </div>
-                  <div className="single-card">
-                      <i>Icon</i>
-                      <h4>Advanced Schedule</h4>
-                  </div>
-                  <div className="single-card">
-                      <i>Icon</i>
-                      <h4>Manage Schedule</h4>
-                  </div>
+              <div className="single-card">
+                <i className="wpsp-icon wpsp-manage-pro"></i>
+                  <h4>Advanced Schedule</h4>
+              </div>
+              <div className="single-card">
+                  <i className="wpsp-icon wpsp-missed-pro"></i>
+                  <h4>Manage Schedule</h4>
               </div>
           </div>
-        ) }
-      </>
->>>>>>> Stashed changes
+        </div>
+    ) }
+    </>
   );
 };
 
