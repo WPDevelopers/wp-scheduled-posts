9327fdaa1a7441be76d15e9adfde490bef971722
shuvo247 <78678198+shuvo247@users.noreply.github.com>
2023-07-30 19:06:39 +0600

    add scheduling option in section widget

--- includes/Admin.php
+++ includes/Admin.php
@@ -30,7 +30,56 @@ class Admin
 	    }
 
 	    add_action( 'wp_ajax_wpsp_el_editor_form', [ $this, 'wpsp_el_tab_action' ] );
+        
+        // Add scheduling tab for section
+        add_action( 'elementor/element/section/section_advanced/after_section_end', [ $this, 'register_controls' ] );
+
+        // // Get section tab data
+        // add_action( 'elementor/element/section/section_advanced/after_section_end', [ $this, 'my_custom_control_callback' ],10, 3 );
+
     }
+
+    public function register_controls( $element ) {
+		$element->start_controls_section(
+			'wpsp_section_schedule',
+			[
+				'label' => __( 'Section Schedule', 'wp-scheduled-posts' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_LAYOUT
+			]
+		);
+
+        $element->add_control(
+			'wpsp_section_publish_on',
+			[
+				'label' => esc_html__( 'Publish On', 'wp-scheduled-posts' ),
+				'type' => \Elementor\Controls_Manager::DATE_TIME,
+			]
+		);
+        $element->add_control(
+			'wpsp_section_republish_on',
+			[
+				'label' => esc_html__( 'Republish On', 'wp-scheduled-posts' ),
+				'type' => \Elementor\Controls_Manager::DATE_TIME,
+			]
+		);
+        $element->add_control(
+			'wpsp_section_unpublish_on',
+			[
+				'label' => esc_html__( 'Unpublish On', 'wp-scheduled-posts' ),
+				'type' => \Elementor\Controls_Manager::DATE_TIME,
+			]
+		);
+
+		$element->end_controls_section();
+	}
+
+    public function content_render( $should_render ) {
+		// $settings = $element->get_settings_for_display();
+        // echo "<pre>";
+        // print_r('Hello',$settings);
+		return false;
+	}
+
     public function load_plugin_menu_pages()
     {
         new Admin\Menu();
