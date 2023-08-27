05902b5a5d38d8c20758d4bfcf405092bcd28274
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2023-08-27 15:02:01 +0600

    updated banner

---
+++ includes/Admin/Settings/assets/js/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'e1c0c9400458081beafe');
