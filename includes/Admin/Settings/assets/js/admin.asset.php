5a31b68a7874a119d2ec510ef471fb5c766e4168
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2023-11-20 16:06:55 +0600

    Revert "working on license activation"
    This reverts commit d9adc62624d0b206e6f724815eee7d9660da583c.

--- includes/Admin/Settings/assets/js/admin.asset.php
+++ includes/Admin/Settings/assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'db3e98bb7faa563e5baf');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '3c211479c7f34247ab8f');
