2e5c62c57c4197b4678226f3eefe4a53cde2c214
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2023-12-06 16:45:34 +0600

    updated priyomukul/wp-notice

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'schedulepress/wp-schedule-posts',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'eb5c614152099b4953ffde348fd6cb08b7d4c1b4',
+        'pretty_version' => 'dev-latest',
+        'version' => 'dev-latest',
+        'reference' => 'c5c94750498caa017f1bfabe0cb5f05af0a179e9',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -67,7 +67,7 @@
         'priyomukul/wp-notice' => array(
             'pretty_version' => 'v2.x-dev',
             'version' => '2.9999999.9999999.9999999-dev',
-            'reference' => 'f3d02f6e772cb459e9b89d77605e02646f9c5d65',
+            'reference' => '20b6e71d94151f6456e7d5b5bd902ab33822b2fb',
             'type' => 'library',
             'install_path' => __DIR__ . '/../priyomukul/wp-notice',
             'aliases' => array(),
@@ -98,9 +98,9 @@
             'dev_requirement' => false,
         ),
         'schedulepress/wp-schedule-posts' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'eb5c614152099b4953ffde348fd6cb08b7d4c1b4',
+            'pretty_version' => 'dev-latest',
+            'version' => 'dev-latest',
+            'reference' => 'c5c94750498caa017f1bfabe0cb5f05af0a179e9',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
