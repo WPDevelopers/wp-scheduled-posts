7dbdd1dfcd911ce73827a481f02067ac4c5bffc7
MD Shuvo <66669067+shuvo7670@users.noreply.github.com>
2024-01-15 09:37:31 +0600

    elementor option issue fixing

--- includes/Admin.php
+++ includes/Admin.php
@@ -271,7 +271,36 @@ class Admin
             #schedulepress-elementor-modal.elementor-templates-modal .dialog-widget-content {
                 background: #f5f7fd;
             }
-
+            .elementor-panel-footer-wpsp-modal .wpsp-elementor-topbar-panel-button:hover {
+                background-color: #3e3e3e;
+            }
+            .elementor-panel-footer-wpsp-modal .wpsp-elementor-topbar-panel-button {
+                display: inline-flex;
+                -webkit-box-align: center;
+                align-items: center;
+                -webkit-box-pack: center;
+                justify-content: center;
+                position: relative;
+                box-sizing: border-box;
+                -webkit-tap-highlight-color: transparent;
+                background-color: transparent;
+                outline: 0px;
+                border: 0px;
+                margin: 0px;
+                cursor: pointer;
+                user-select: none;
+                vertical-align: middle;
+                appearance: none;
+                text-decoration: none;
+                text-align: center;
+                flex: 0 0 auto;
+                font-size: 1.5rem;
+                padding: 8px;
+                border-radius: 50%;
+                overflow: visible;
+                color: rgb(255, 255, 255);
+                transition: background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+            }
             .wpsp-el-modal-date-picker .flatpickr-calendar {
                 left: 50% !important;
                 top: 50% !important;
