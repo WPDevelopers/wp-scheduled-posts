8d06e6769a57dc53bdea6a69e4f904854335a031
MD Shuvo <66669067+shuvo7670@users.noreply.github.com>
2024-02-28 15:05:54 +0600

    nonce version issue fixed

--- includes/Social/InstantShare.php
+++ includes/Social/InstantShare.php
@@ -268,10 +268,19 @@ class InstantShare
      */
     public function instant_share_fetch_profile()
     {
-        if( !current_user_can('manage_options') ) {
-            wp_send_json_error( [ 'message' => __('You are unauthorized to access social profiles.', 'wp-scheduled-posts') ], 401 );
-            wp_die();
-        }
+        
+         // Verify nonce
+         $nonce = sanitize_text_field($_REQUEST['_nonce']);
+         if ( !wp_verify_nonce($nonce, basename(__FILE__)) ) {
+             wp_send_json_error( ['message' => __('Invalid nonce.', 'wp-scheduled-posts') ] );
+             die();
+         }
+ 
+         if( !current_user_can('manage_options') ) {
+             wp_send_json_error( [ 'message' => __('You are unauthorized to access social profiles.', 'wp-scheduled-posts') ], 401 );
+             wp_die();
+         }
+
         $allProfile = array();
         $facebook_selected_profiles  = !empty( $_REQUEST['facebook_selected_profiles'] ) ? array_map( 'sanitize_text_field', $_REQUEST['facebook_selected_profiles'] ) : [];
         $twitter_selected_profiles   = !empty( $_REQUEST['twitter_selected_profiles'] ) ? array_map( 'sanitize_text_field', $_REQUEST['twitter_selected_profiles'] ) : [];
