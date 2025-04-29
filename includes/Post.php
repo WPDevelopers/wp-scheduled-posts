cd60ae82fb23873a911177b5ea907de02778c545
Kazi Sourav <32020708+khmsourav@users.noreply.github.com>
2025-04-29 13:07:12 +0600

    title gap related issues fixed

--- includes/Post.php
+++ includes/Post.php
@@ -93,7 +93,7 @@ class Post
                                 <div class="wpsp-post-items--wrapper">
                                     <div class="wpsp-post--items">
                                         <div class="card--title">
-                                            <h5 class="title">Auto Schedule</h5>
+                                            <h5 class="title">Schedule On</h5>
                                             <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <circle cx="9" cy="9.5" r="9" fill="#FFEEE0"/>
                                                 <path d="M4.06398 11.3651C3.83586 9.88232 3.60775 8.39961 3.37964 6.91686C3.32905 6.58817 3.70304 6.364 3.96906 6.56353C4.67976 7.09656 5.39043 7.62955 6.10113 8.16257C6.33513 8.33807 6.6685 8.28096 6.83073 8.03758L8.60571 5.37508C8.79329 5.09372 9.20669 5.09372 9.39426 5.37508L11.1693 8.03758C11.3315 8.28096 11.6649 8.33803 11.8989 8.16257C12.6096 7.62955 13.3202 7.09656 14.0309 6.56353C14.2969 6.364 14.6709 6.58817 14.6204 6.91686C14.3923 8.39961 14.1642 9.88232 13.936 11.3651H4.06398Z" fill="#FFA454"/>
@@ -108,7 +108,7 @@ class Post
                                     </div>
                                     <div class="wpsp-post--items">
                                         <div class="card--title">
-                                            <h5 class="title">Auto Schedule</h5>
+                                            <h5 class="title">Manual Schedule</h5>
                                             <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <circle cx="9" cy="9.5" r="9" fill="#FFEEE0"/>
                                                 <path d="M4.06398 11.3651C3.83586 9.88232 3.60775 8.39961 3.37964 6.91686C3.32905 6.58817 3.70304 6.364 3.96906 6.56353C4.67976 7.09656 5.39043 7.62955 6.10113 8.16257C6.33513 8.33807 6.6685 8.28096 6.83073 8.03758L8.60571 5.37508C8.79329 5.09372 9.20669 5.09372 9.39426 5.37508L11.1693 8.03758C11.3315 8.28096 11.6649 8.33803 11.8989 8.16257C12.6096 7.62955 13.3202 7.09656 14.0309 6.56353C14.2969 6.364 14.6709 6.58817 14.6204 6.91686C14.3923 8.39961 14.1642 9.88232 13.936 11.3651H4.06398Z" fill="#FFA454"/>
@@ -137,7 +137,7 @@ class Post
                                 </div>
                                 <div class="wpsp-post-items--wrapper">
                                     <div class="wpsp-post--items">
-                                        <div class="card--title">
+                                        <div class="card--title info">
                                             <h5 class="title">Unpublish On</h5>
                                             <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <g clip-path="url(#clip0_3532_3956)">
@@ -159,7 +159,7 @@ class Post
                                         </div>
                                     </div>
                                     <div class="wpsp-post--items">
-                                        <div class="card--title">
+                                        <div class="card--title info">
                                             <h5 class="title">Republish On</h5>
                                             <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <g clip-path="url(#clip0_3532_3956)">
@@ -183,7 +183,7 @@ class Post
                                 </div>
                                 <div class="wpsp-tag--wrapper">
                                     <div class="card--title">
-                                        <h5 class="title">Republish On</h5>
+                                        <h5 class="title">Add Custom Tag</h5>
                                     </div>
                                     <div class="tag-item--wrapper">
                                         <div class="tag--item">
@@ -220,8 +220,8 @@ class Post
                                     <div class="advance-schedule--items">
                                         <div class="wpsp-post-items--wrapper">
                                             <div class="wpsp-post--items">
-                                                <div class="card--title">
-                                                    <h5 class="title">Unpublish On</h5>
+                                                <div class="card--title info">
+                                                    <h5 class="title">Publish Date</h5>
                                                     <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <g clip-path="url(#clip0_3532_3956)">
                                                         <circle cx="6.99935" cy="6.99935" r="5.83333" stroke="#667085" stroke-width="1.2"/>
