9c1db874ed20c8468a0d7634d9ee05513035f035
shuvo7670 <mdshakibul.shuvo@gmail.com>
2025-10-29 14:25:02 +0600

    Update admin and calendar assets
    Refreshed CSS and asset files for admin and calendar components to improve UI consistency and performance. Updated translation template to reflect new strings.

--- includes/Admin/Settings/assets/js/calendar.asset.php
+++ includes/Admin/Settings/assets/js/calendar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => '8beeb671b25f107fe997');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => '8143efbab4fdba6c9c55');
