bb18a454bceea662524cc2275ea93c432528821b
MD Shuvo <66669067+shuvo7670@users.noreply.github.com>
2026-03-15 00:54:29 +0600

    Update AllDisabledPlatform.js

--- src/components/modals/socialTemplates/AllDisabledPlatform.js
+++ src/components/modals/socialTemplates/AllDisabledPlatform.js
@@ -4,13 +4,22 @@ const AllDisabledPlatform = ({ platforms }) => {
     return (
         <div className="wpsp-platform-icons all-profiles-disabled">
             {platforms.map(({ platform, icon }) => (
-                <button
-                    key={platform}
-                    className={`wpsp-platform-icon disabled`}
-                    title={platform}
-                >
-                    {icon}
-                </button>
+                <div key={platform} className="wpsp-tooltip-wrapper">
+                    <div className={`wpsp-platform-icon-button-wrapper ${platform}`}>
+                        <button
+                            key={platform}
+                            className={`wpsp-platform-icon disabled`}
+                            title={platform}
+                        >
+                            {icon}
+                        </button>
+
+                        <div className="wpsp-tooltip">
+                            Not connected yet. <br/>
+                            <span>Connect a social account from <br/> SchedulePress → Social Profiles.</span>
+                        </div>
+                    </div>
+                </div>
             ))}
             <p className="wpsp-disabled-message">*Connect accounts from SchedulePress → Social Profiles to enable sharing.</p>
         </div>
