fd1dfdd2b0c94a15fc7fce60f7731331635ae951
Kazi Sourav <32020708+khmsourav@users.noreply.github.com>
2026-04-28 15:48:24 +0600

    Post panel model layout related UI

--- src/components/common/Content.js
+++ src/components/common/Content.js
@@ -3,9 +3,11 @@ import Settings from '../Content/Settings';
 import SocialShare from '../Content/SocialShare';
 const Content = () => {
     return (
-        <div className="wpsp-post-panel-modal-content">
-            <Settings />
-            <SocialShare />
+        <div className="wpsp-post-panel-modal-content-wrap">
+            <div className="wpsp-post-panel-modal-content">
+                <Settings />
+                <SocialShare />
+            </div>
         </div>
     );
 };
