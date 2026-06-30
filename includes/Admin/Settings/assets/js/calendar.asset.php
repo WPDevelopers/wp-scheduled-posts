c9cbe779f1eda0d5d9f80fb16b85d1904bd11c30
shuvo7670 <mdshakibul.shuvo@gmail.com>
2026-06-30 15:02:38 +0600

    Rebuild assets: styles, admin & calendar
    Regenerated compiled assets and updated source styles and admin/calendar scripts. Changes include updated app.min.css and app.min.js, refreshed admin and calendar CSS/JS and their asset manifests, updated SCSS source (src/scss/styles.scss), and refreshed translation template (languages/wp-scheduled-posts.pot). These updates reflect style tweaks and an assets rebuild for the admin/settings UI and calendar components.

--- includes/Admin/Settings/assets/js/calendar.asset.php
+++ includes/Admin/Settings/assets/js/calendar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => '7ed92c84cabd3c7f51ef');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => '6d949fced64d83f493dc');
