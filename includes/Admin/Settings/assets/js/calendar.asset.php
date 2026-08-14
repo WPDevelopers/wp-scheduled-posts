208991a0414c9448ff35eb4bbad468f4e6f8fa23
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2026-08-14 11:36:03 +0600

    Update asset versions for admin and calendar JavaScript files

--- includes/Admin/Settings/assets/js/calendar.asset.php
+++ includes/Admin/Settings/assets/js/calendar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => 'aba1cd0009104b6637e8');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => 'edfcb89ba3c4e247b926');
